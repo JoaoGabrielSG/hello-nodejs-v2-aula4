5587aa61782b5c9aa655168f7368c82feb39c50f
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2016-11-23 22:48:47 -0300

    end class

---
+++ 4.1-knex-js-sqlite-postgresql/intro-knex/hello-knex-1.js
@@ -0,0 +1,24 @@
+// const knex = require("knex")({
+//   client: "sqlite3",
+//   connection: {
+//     filename: "./hello.sqlite"
+//   },
+//   useNullAsDefault: true // coisas de sqlite.. ¯\_(ツ)_/¯
+// });
+
+"use strict"
+
+const knex = require("./db");
+
+
+// criar uma tabela
+knex.schema.createTable("convidado", (table) =>{
+  table.increments("idconvidado");
+  table.string("nomeconvidado");
+}).then(() => {
+  console.log("tabela criada");
+  process.exit(0);
+}).catch((err)=>{
+  console.log(err);
+  process.exit(1);
+});
